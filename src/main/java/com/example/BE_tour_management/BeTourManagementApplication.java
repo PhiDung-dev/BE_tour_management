16cf4f103ed024c5aefa76d4c03730880d2f33f9
Phi Dung <phidung.dev@gmail.com>
2026-03-28 18:11:02 +0700

    init project

---
+++ src/main/java/com/example/BE_tour_management/BeTourManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.BE_tour_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTourManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTourManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BE_tour_management/BeTourManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_tour_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTourManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
